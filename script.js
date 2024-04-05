6bae6d16a440446b4cdc61ac6d3bf12a56152e21
Herr Prof <farouqoguntoye05@gmail.com>
2024-04-06 00:34:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const textareaEl = document.getElementById("textarea")
+const totalCounterEl = document.getElementById("total-counter")
+const remCounterEl = document.getElementById("rem-counter")
+
+textareaEl.addEventListener("keyup", ()=>{
+    updateCounter()
+})
+
+updateCounter()
+
+function updateCounter(){
+    totalCounterEl.innerText = textareaEl.value.length
+    remCounterEl.innerText = textareaEl.getAttribute("maxlength") - textareaEl.value.length
+}
